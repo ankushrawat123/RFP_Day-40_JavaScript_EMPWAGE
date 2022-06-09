63996855c6adc6ab2315519be4e34ca1c0dd3c74
AnkushRawat <ankushrwt344@gmail.com>
2022-06-10 01:49:50 +0530

    adding employee attendance checking file

---
+++ EmpWage_JavaScript.js
@@ -0,0 +1,11 @@
+//UC1 using if else
+{
+    const isAbsent = 0;
+    let num = Math.floor(Math.random() * 10) % 2;
+    if (num == isAbsent) {
+        console.log("Employee is Absent");
+    }
+    else {
+        console.log("Employee is Present");
+    }
+}
