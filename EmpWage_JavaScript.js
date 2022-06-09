c82ee0a5b238fe5c26a9a85abcebc01ff880e307
AnkushRawat <ankushrwt344@gmail.com>
2022-06-10 01:54:27 +0530

    uploading function javascript empwage file

--- EmpWage_JavaScript.js
+++ EmpWage_JavaScript.js
@@ -1,26 +1,24 @@
-//UC2 using switch case
+//UC3 using Function
 {
-    const isAbsent = 0;
-    const partTimeEmp = 1;
-    const fullTimeEmp = 2;
+
+    const isPartTime = 1;
+    const isFullTime = 2;
     const partTimeHour = 4;
     const fullTimeHour = 8;
     const wagePerHour = 20;
-
-    let empHrs = 0;
-    let num = Math.floor(Math.random() * 10) % 3;
-    switch (num) {
-
-        case partTimeEmp:
-            empHrs = partTimeHour;
-            break;
-        case fullTimeEmp:
-            empHrs = fullTimeHour;
-            break;
-        default:
-            empHrs = 0;
-
+    function empHrsFinder(num) {
+        switch (num) {
+            case isPartTime:
+                return partTimeHour;
+            case isFullTime:
+                return fullTimeHour;
+            default:
+                return 0;
+        }
     }
-    let empWage = wagePerHour * empHrs;
-    console.log("Employee wage is " + empWage);
+    let num = Math.floor(Math.random() * 10) % 3;
+    let empHrs = empHrsFinder(num);
+    let empWage = empHrs * wagePerHour;
+    console.log("Employee Wage : " + empWage + " Employee Hours : " + empHrs);
 }
+
