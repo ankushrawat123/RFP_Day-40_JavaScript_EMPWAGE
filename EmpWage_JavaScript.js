25eb667d25ab4c1f1f85d8b081cff9f635ca6cf7
AnkushRawat <ankushrwt344@gmail.com>
2022-06-10 01:57:57 +0530

    Uploading whileLoop empwage Javascript file

--- EmpWage_JavaScript.js
+++ EmpWage_JavaScript.js
@@ -1,30 +1,39 @@
-//UC4 using for loop
+//UC5 using While loop
 {
-    const numOfWorkingDays = 20;
-    const partTimeEmp = 1;
-    const fullTimeEmp = 2;
-    const partTimeHour = 4;
-    const fullTimeHour = 8;
-    const wagePerHour = 20;
 
-    let totalEmpHours = 0;
+    const maxEmpHrs = 160;
+    const numOfWorkingDays=20;
+    const partTimeEmp =1;
+    const fullTimeEmp =2;
+    const partTimeHour=4;
+    const fullTimeHour=8;
+    const wagePerHour=20;
+    
+    let totalEmpHours=0;
+    let totalEmpDays=0;
 
-    function empHrsFinder(num) {
-        switch (num) {
-            case partTimeEmp:
-                return partTimeHour;
+    function empHrsFinder(num)
+    {
+
+        switch(num)
+        {
+        case partTimeEmp:
+            return partTimeHour;
             case fullTimeEmp:
                 return fullTimeHour;
-            default:
-                return 0;
+                default:
+                    return 0;
         }
-    }
 
-    for (let day = 0; day <= numOfWorkingDays; day++) {
-        let num = Math.floor(Math.random() * 10) % 3;
-        totalEmpHours += empHrsFinder(num);;
     }
-    let empWage = totalEmpHours * wagePerHour;
-    console.log("Total Employee Hours : " + totalEmpHours + " Employee Wage " + empWage);
+    
+    while(totalEmpHours<=maxEmpHrs && totalEmpDays<numOfWorkingDays)
+    {
+        let num = Math.floor(Math.random()*10)%3;
+        let empHrs = empHrsFinder(num);
+        totalEmpHours += empHrs;
+        totalEmpDays++;
+    }
+    let empWage = wagePerHour*totalEmpHours;
+    console.log("Working Hours " + totalEmpHours + " Total Employee Wage " + empWage);
 }
-
